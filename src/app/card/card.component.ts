1437383d03c2a4f1f02aa9a1b409ebabc16633ad
Renan <orefusado@gmail.com>
2022-11-09 17:26:13 -0300

    melhoras/refatoração e finalizado sistemas do card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,62 +1,69 @@
-import { style } from '@angular/animations';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
-  products: string[] = [];
-  showingBtn: boolean = true;
+export class CardComponent {
+  @Input() cardName: string = 'Undefined';
 
-  @Input() type: string = 'Undefined';
+  listItems: string[] = [];
+  newItemText: string = "Tarefa";
+
+  cardDescription: string = "Escreva uma descrição do seu projeto clicando aqui.";
+
+  showingAddBtn: boolean = true;
+  editingDescription: boolean = false;
 
   constructor() {
-    this.products = [
-      'tarefa 1',
-      'tarefa 2...'
+    this.listItems = [
+      'Criar README.md',
+      'Configurar deploy'
     ]
-  }
-
-  ngOnInit(): void {
-  }
-
-  removeItem(index: number): void {
-    this.products.splice(index, 1);
   }
 
   addItem(): void {
     if (this.newItemText) {
-      this.products.push(this.newItemText);
-      this.newItemText = "Nova tarefa";
+      this.listItems.push(this.newItemText);
+      this.newItemText = "Tarefa";
     }
   }
 
-  newItemText: string = "Nova tarefa";
-  cardDescription: string = "Este é o card para um usuário comum e possue apenas algumas ferramentas";
-  editingDescription: boolean = false;
-
-  editDescription(): void {
-    let input = document.getElementById('descriptionInput');
-
-    if (this.editingDescription) {
-      this.editingDescription = false;
-
-      if (input) {
-        input.focus();
-
-        const range = document.createRange();
-        range.setStart(input, 0);
-      }
-
-    } else {
-      this.editingDescription = true;
-    }
+  removeItem(index: number): void {
+    this.listItems.splice(index, 1);
   }
 
-  preventNewLine(event: Event): void {
-    event.preventDefault();
+  openInput(): void {
+    if (!this.editingDescription)
+      setTimeout(() => {
+        this.editingDescription = true;
+        console.log('aberto');
+      }, 10);
   }
 
+  closeInput(): void {
+    this.editingDescription = false;
+    console.log('fechado');
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: any) {
+    const elem = event.target;
+    const input = document.getElementById('descriptionInput');
+    const span = document.getElementById('cardDescription');
+
+    if (this.editingDescription && (elem != input) && (elem != span))
+      this.closeInput();
+  }
+
+  preventTyping(event: any): void {
+    const verifySpace: RegExp =  /\s$/gm;
+
+    if (event.key == "Enter")
+      event.preventDefault();
+
+    if (event.key == " " && verifySpace.test(event.target.value))
+      event.preventDefault();
+  }
 }
